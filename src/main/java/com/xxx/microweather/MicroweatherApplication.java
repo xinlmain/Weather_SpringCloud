c15822c92cd9c58aea5bb35b4e027418757a0227
xinl <xinlmain@163.com>
2018-08-29 23:40:25 +0800

    初次提交

---
+++ src/main/java/com/xxx/microweather/MicroweatherApplication.java
@@ -0,0 +1,12 @@
+package com.xxx.microweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroweatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroweatherApplication.class, args);
+    }
+}
---
+++ src/test/java/com/xxx/microweather/MicroweatherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xxx.microweather;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroweatherApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
